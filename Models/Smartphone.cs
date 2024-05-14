9e98433955268233b5eefbcd44a8df06ebe38e80
Guilherme-Pereira-Dos-Santos <guilhermepereira280207@gmail.com>
2024-05-14 17:44:21 -0300

    Sistema de celular

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,44 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
 
-        public Smartphone(string numero)
+        private string Memoria { get; set; }
+
+        
+        public Smartphone(string numero, string modelo, string imei, string memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        protected Smartphone(string numero)
+        {
+            Numero = numero;
+        }
+
+        public string ExibirModelo()
+        {
+            return Modelo;
+        }
+
+        public string ExibirIMEI()
+        {
+            return IMEI;
+        }
+
+        public string ExibirMemoria()
+        {
+            return Memoria;
         }
 
         public void Ligar()
